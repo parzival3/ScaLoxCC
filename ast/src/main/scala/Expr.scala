3ccc31c2c91d9767efcd430e60d9c35741de9323
Enrico Tolotto <etolotto@gmail.com>
2021-04-22 21:28:06 +0200

    src: all update

--- ast/src/main/scala/Expr.scala
+++ ast/src/main/scala/Expr.scala
@@ -3,9 +3,11 @@ package io.github.parzival3.scaloxcc.ast
 sealed trait Expr
 case class Unary(token: TokenInstance, expr: Expr) extends Expr
 case class Binary(left: Expr, token: TokenInstance, right: Expr) extends Expr
-case class Call(name: TokenInstance, paren: TokenType, arguments: java.util.List[Expr]) extends Expr
+case class Call(name: TokenInstance, arguments: java.util.List[TokenInstances]) extends Expr
 case class Grouping(expr: Expr) extends Expr
 case class Literal(value: AnyVal) extends Expr
 case class Assign(value: TokenInstance, expr: Expr) extends Expr
 case class Variable(name: TokenInstance) extends Expr
 case class Logical(left: Expr, token: TokenInstance, right: Expr) extends Expr
+case class Set(obj: Expr, name: TokenInstance, value: Expr) extends Expr
+case class Get(obj: Expr, name: TokenInstance) extends Expr
--- ast/src/main/scala/TokenType.scala
+++ ast/src/main/scala/TokenType.scala
@@ -2,23 +2,103 @@ package io.github.parzival3.scaloxcc.ast
 
 // This is necessary to use the enum in Java
 // https://dotty.epfl.ch/docs/reference/enums/enums.html#compatibility-with-java-enums
-enum TokenType extends Enum[TokenType] {
+enum TokenType(lexeme: String) extends Enum[TokenType]:
   // Single-character tokens.
-  case LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  case LEFT_PAREN extends TokenType("(")
+  case RIGHT_PAREN extends TokenType(")")
+  case LEFT_BRACE extends TokenType("{")
+  case RIGHT_BRACE extends TokenType("}")
+  case COMMA extends TokenType(",")
+  case DOT extends TokenType(".")
+  case MINUS extends TokenType("-")
+  case PLUS extends TokenType("+")
+  case SEMICOLON extends TokenType(";")
+  case SLASH extends TokenType("/")
+  case STAR extends TokenType("*")
 
   // One or two character tokens.
-  BANG, BANG_EQUAL,
-  EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL,
-  LESS, LESS_EQUAL,
+  case BANG extends TokenType("!")
+  case BANG_EQUAL extends TokenType("!=")
+  case EQUAL extends TokenType("=")
+  case EQUAL_EQUAL extends TokenType("==")
+  case GREATER extends TokenType(">")
+  case GREATER_EQUAL extends TokenType(">=")
+  case LESS extends TokenType("<")
+  case LESS_EQUAL extends TokenType("<=")
 
   // Literals.
-  IDENTIFIER, STRING, NUMBER,
+  case IDENTIFIER extends TokenType("IDENTIFIER")
+  case STRING extends TokenType("STRING")
+  case NUMBER extends TokenType("NUMBER")
 
   // Keywords.
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+  case AND extends TokenType("and")
+  case CLASS extends TokenType("class")
+  case ELSE extends TokenType("else")
+  case FALSE extends TokenType("false")
+  case FUN extends TokenType("fun")
+  case FOR extends TokenType("for")
+  case IF extends TokenType("if")
+  case NIL extends TokenType("nil")
+  case OR extends TokenType("or")
+  case PRINT extends TokenType("print")
+  case RETURN extends TokenType("return")
+  case SUPER extends TokenType("super")
+  case THIS extends TokenType("this")
+  case TRUE extends TokenType("true")
+  case VAR extends TokenType("var")
+  case WHILE extends TokenType("while")
 
-  EOF
-}
+  case EOF extends TokenType("EOF")
+
+
+object TokenType:
+
+  def fromLexeme(l: String): TokenType
+      l match
+        case TokenType.LEFT_PAREN.lexeme =>
+        case RIGHT_PAREN extends TokenType(")")
+        case LEFT_BRACE extends TokenType("{")
+        case RIGHT_BRACE extends TokenType("}")
+        case COMMA extends TokenType(",")
+        case DOT extends TokenType(".")
+        case MINUS extends TokenType("-")
+        case PLUS extends TokenType("+")
+        case SEMICOLON extends TokenType(";")
+        case SLASH extends TokenType("/")
+        case STAR extends TokenType("*")
+
+        // One or two character tokens.
+        case BANG extends TokenType("!")
+        case BANG_EQUAL extends TokenType("!=")
+        case EQUAL extends TokenType("=")
+        case EQUAL_EQUAL extends TokenType("==")
+        case GREATER extends TokenType(">")
+        case GREATER_EQUAL extends TokenType(">=")
+        case LESS extends TokenType("<")
+        case LESS_EQUAL extends TokenType("<=")
+
+        // Literals.
+        case IDENTIFIER extends TokenType("IDENTIFIER")
+        case STRING extends TokenType("STRING")
+        case NUMBER extends TokenType("NUMBER")
+
+        // Keywords.
+        case AND extends TokenType("and")
+        case CLASS extends TokenType("class")
+        case ELSE extends TokenType("else")
+        case FALSE extends TokenType("false")
+        case FUN extends TokenType("fun")
+        case FOR extends TokenType("for")
+        case IF extends TokenType("if")
+        case NIL extends TokenType("nil")
+        case OR extends TokenType("or")
+        case PRINT extends TokenType("print")
+        case RETURN extends TokenType("return")
+        case SUPER extends TokenType("super")
+        case THIS extends TokenType("this")
+        case TRUE extends TokenType("true")
+        case VAR extends TokenType("var")
+        case WHILE extends TokenType("while")
+
+        case EOF extends TokenType("EOF")
