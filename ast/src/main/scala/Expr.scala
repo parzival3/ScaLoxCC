6dbfb39d5b3b43ccb3f59310656a57e2a9848d90
Enrico Tolotto <etolotto@gmail.com>
2021-04-22 22:30:14 +0200

    src: update

--- ast/src/main/scala/Expr.scala
+++ ast/src/main/scala/Expr.scala
@@ -5,9 +5,10 @@ case class Unary(token: TokenInstance, expr: Expr) extends Expr
 case class Binary(left: Expr, token: TokenInstance, right: Expr) extends Expr
 case class Call(name: TokenInstance, arguments: java.util.List[TokenInstances]) extends Expr
 case class Grouping(expr: Expr) extends Expr
-case class Literal(value: AnyVal) extends Expr
+case class Literal(value: TokenInstance) extends Expr
 case class Assign(value: TokenInstance, expr: Expr) extends Expr
 case class Variable(name: TokenInstance) extends Expr
 case class Logical(left: Expr, token: TokenInstance, right: Expr) extends Expr
 case class Set(obj: Expr, name: TokenInstance, value: Expr) extends Expr
 case class Get(obj: Expr, name: TokenInstance) extends Expr
+case class This(name: TokenInstance) extends Expr
