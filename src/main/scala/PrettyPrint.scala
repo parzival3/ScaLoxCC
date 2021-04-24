d72e61e62ef0ab0af6dc74aeb7107ca3e7949d4e
Enrico Tolotto <etolotto@gmail.com>
2021-04-24 17:18:55 +0200

    main: comment out pretty print

--- src/main/scala/PrettyPrint.scala
+++ src/main/scala/PrettyPrint.scala
@@ -3,10 +3,9 @@ package io.github.parzival3.scaloxcc.util
 import io.github.parzival3.scaloxcc.ast._
 import io.github.parzival3.scaloxcc.javacc._
 
-def prettyPrint(expression: Expr): String =
-  expression match
-    case Binary(l, t, r) => s"(${t} ${prettyPrint(l)}  ${prettyPrint(r)})"
-    case Unary(t, e) => s"(${t} ${prettyPrint(e)})"
-    case Grouping(e) => s"(group ${prettyPrint(e)})"
-    case Literal(v) => s"$v"
-
+// def prettyPrint(expression: Expr): String =
+//   expression match
+//     case Binary(l, t, r) => s"(${t} ${prettyPrint(l)}  ${prettyPrint(r)})"
+//     case Unary(t, e) => s"(${t} ${prettyPrint(e)})"
+//     case Grouping(e) => s"(group ${prettyPrint(e)})"
+//     case Literal(v) => s"$v"
